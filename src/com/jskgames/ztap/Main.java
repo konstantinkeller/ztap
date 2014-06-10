73d2b1402b14a5eb64a86d0f4f9ba5cf864020f2
konstantinkeller <konstantinkeller5@gmail.com>
2014-06-10 18:15:45 -0400

    Created base project

---
+++ src/com/jskgames/ztap/Main.java
@@ -0,0 +1,18 @@
+package com.jskgames.ztap;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.app.Activity;
+
+public class Main extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        setContentView(R.layout.main);
+    }
+}
