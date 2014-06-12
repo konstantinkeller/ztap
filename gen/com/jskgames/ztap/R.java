1c5ebb1799e67e727ab96f086c891f53069fb5cb
Konstantin Keller <konstantinkeller5@gmail.com>
2014-06-11 21:13:29 -0400

    Added VERY simple main menu and game placeholder

--- gen/com/jskgames/ztap/R.java
+++ gen/com/jskgames/ztap/R.java
@@ -25,13 +25,20 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ball=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
+    public static final class id {
+        public static final int start_game=0x7f070001;
+        public static final int textView1=0x7f070000;
+        public static final int title=0x7f070002;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int game=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int dummy_button=0x7f050001;
-        public static final int dummy_content=0x7f050002;
+        public static final int bg_placeholder=0x7f050002;
+        public static final int game_placeholder=0x7f050003;
+        public static final int start_button=0x7f050001;
     }
     public static final class style {
         /** 
--- src/com/jskgames/ztap/Main.java
+++ src/com/jskgames/ztap/Main.java
@@ -3,14 +3,30 @@ package com.jskgames.ztap;
 import android.os.Bundle;
 import android.view.View;
 import android.app.Activity;
+import android.content.Intent;
 
 public class Main extends Activity {
+		
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        getWindow().getDecorView().setSystemUiVisibility(
+        		View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         setContentView(R.layout.main);
+        
     }
+    
+    public void startGame(View v) {
+    	Intent startGameIntent = new Intent(Main.this, Game.class);
+    	Main.this.startActivity(startGameIntent);
+    }
+
 }
