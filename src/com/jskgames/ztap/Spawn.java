8d6a67fe1ecc5b715ce58cbf7fa08609286033e6
DollarPlus <jasper_shan@hotmail.com>
2014-06-13 07:14:00 -0400

    added temp placeholder functions for lengthGen and moveSpeed.

--- src/com/jskgames/ztap/Spawn.java
+++ src/com/jskgames/ztap/Spawn.java
@@ -8,11 +8,11 @@ public class Spawn {
 	
 	// Generates an array of integers that represents the sequence of directions
 	// for a newly generated enemy.
-	public static int[] sequenceGen(int n, int start) {
-		int[] seq = new int[n];
+	public static int[] sequenceGen(int length, int start) {
+		int[] seq = new int[length];
 		seq[0] = start;
 		
-		for (int i = 1; i <= n-1; i++) {
+		for (int i = 1; i <= length-1; i++) {
 			seq[i] = randInt(1,8);
 		}
 		
@@ -23,6 +23,25 @@ public class Spawn {
 	public static int randInt(int min, int max) {
 		int randomNum = rand.nextInt((max - min) + 1) + min;
 		return randomNum;
+	}
+	
+	// Generates length of sequence based on the level
+	// Resets length per 10 levels
+	public static int lengthGen (int level)
+	{
+		if (level % 10 == 0)
+			return 10;
+		else
+			return (level + 4);
+	}
+	
+	// Movespeed of individual enemy. Will be tested further
+	// Placeholder for now.
+	public static int moveSpeed (int level)
+	{
+		int moveSpeedX = 5 * level;
+		int moveSpeedY = 5 * level;
+		return 1;
 	}
 
 }
